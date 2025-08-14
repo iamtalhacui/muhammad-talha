4217aae34c0f3e3f519b69c89e2e61850344b2ac
Muhammad Talha <mc@MCs-MacBook-Pro.local>
2025-08-14 01:48:28 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // tells Next.js to generate static HTML
+  basePath: "/my-portfolio", // your repo name
+  images: {
+    unoptimized: true // needed for GitHub Pages
+  }
+};
 
 export default nextConfig;
